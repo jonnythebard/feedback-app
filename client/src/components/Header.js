6911f336cfd0104b7c2047225f30ff1fead10c12
jonnythebard <jonnythebard9@gmail.com>
2018-01-11 17:46:29 +0900

    setup billing process with stripe api

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux"
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
     renderContent() {
@@ -9,16 +10,13 @@ class Header extends Component {
                 return;
             case false:
                 return (
-                    <li>
-                        <a href="/auth/google">Login with google</a>
-                    </li>
+                    <li><a href="/auth/google">Login with google</a></li>
                 );
             default:
-                return (
-                    <li>
-                        <a href="/api/logout">Log out</a>
-                    </li>
-                );
+                return [
+                    <li><Payments /></li>,
+                    <li><a href="/api/logout">Log out</a></li>
+                ];
         }
     }
 
