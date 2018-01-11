c5b97bdc6bb39d634d7a47f22b257bc2c99ab36e
jonnythebard <jonnythebard9@gmail.com>
2018-01-11 15:32:35 +0900

    complete auth flow of the app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
+import { connect } from "react-redux"
+import { Link } from "react-router-dom";
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google">Login with google</a>
+                    </li>
+                );
+            default:
+                return (
+                    <li>
+                        <a href="/api/logout">Log out</a>
+                    </li>
+                );
+        }
+    }
+
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">Emaily</a>
+                    <Link to={this.props.auth ? "/surveys" : "/"}
+                          className="left brand-logo"
+                    >
+                        Emaily
+                    </Link>
                     <ul className="right">
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -17,4 +40,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps(state) {
+    return { auth: state.auth }
+}
+
+export default connect(mapStateToProps)(Header);
