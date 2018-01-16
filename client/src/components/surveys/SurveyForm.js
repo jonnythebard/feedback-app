59c0fe52051cd812536b39927ee409f985930f1a
jonnythebard <jonnythebard9@gmail.com>
2018-01-16 15:31:31 +0900

    setup redux form

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { reduxForm, Field } from "redux-form";
+
+class SurveyForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <form
+                    onSubmit={this.props.handleSubmit(values => console.log(values))}
+                >
+                    <Field
+                        type="text"
+                        name="surveyTitle"
+                        component="input"
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default reduxForm({
+    form: "surveyForm"
+})(SurveyForm);
