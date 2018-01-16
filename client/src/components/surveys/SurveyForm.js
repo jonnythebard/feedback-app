567fb9540c678bf7a3d58d4dbadc7e6a4f1df419
jonnythebard <jonnythebard9@gmail.com>
2018-01-16 16:42:24 +0900

    add form fields with css style

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,21 +1,39 @@
 import React from "react";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
+import _ from "lodash"
+import SurveyField from "./SurveyField";
+
+const FIELDS = [
+    { label: "Survey Title", name: "title" },
+    { label: "Subject", name: "subject" },
+    { label: "Email Body", name: "body" },
+    { label: "Recipient list", name: "list" },
+];
 
 class SurveyForm extends React.Component {
+    renderFields() {
+        return _.map(FIELDS, ({ label, name }) => {
+            return (
+                <Field  key={name} component={SurveyField} type="text" label={label} name={name} />
+            );
+        })
+    }
+
     render() {
         return (
-            <div>
-                <form
-                    onSubmit={this.props.handleSubmit(values => console.log(values))}
-                >
-                    <Field
-                        type="text"
-                        name="surveyTitle"
-                        component="input"
-                    />
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+            <form
+                onSubmit={this.props.handleSubmit(values => console.log(values))}
+            >
+                {this.renderFields()}
+                <Link to="/surveys" className="red btn-flat white-text">
+                    cancel
+                </Link>
+                <button type="submit" className="teal btn-flat right white-text">
+                    next
+                    <i className="material-icons right">done</i>
+                </button>
+            </form>
         );
     }
 }
